6b1484fdb34f67da62ee1b029c96432e2d2ec357
Leonard Law <84147337+llaw-hopper@users.noreply.github.com>
2024-12-02 15:03:03 -0500

    feat: payment flow (#3)
    * chore: introduce flow-specific payment models
    * feat: add tokenization credit card model

---
+++ src/Com.Hopper.Hts.Airlines/Flow/CfarFlow.cs
@@ -0,0 +1,88 @@
+using Com.Hopper.Hts.Airlines.Flow.Model;
+using Com.Hopper.Hts.Airlines.Spreedly.Api;
+using Com.Hopper.Hts.Airlines.Api;
+using ApiModel = Com.Hopper.Hts.Airlines.Model;
+using System.Collections.Generic;
+using Com.Hopper.Hts.Airlines.Spreedly.Model;
+
+namespace Com.Hopper.Hts.Airlines.Flow
+{
+    public interface ICfarFlow
+    {
+        ApiModel.CfarContract UpdateCfarContractWithFormsOfPayment(string contractId, UpdateCfarContractFormsOfPaymentRequest request, string? sessionId);
+    }
+
+    public partial class CfarFlow : ICfarFlow
+    {
+        public CfarFlow(PaymentApi payment, Encryption encryption, CancelForAnyReasonCFARApi cfar)
+        {
+            this.PaymentApi = payment;
+            this.Encryption = encryption;
+            this.CfarApi = cfar;
+        }
+
+        public PaymentApi PaymentApi { get; set; }
+        public Encryption Encryption { get; set; }
+        public CancelForAnyReasonCFARApi CfarApi { get; set; }
+
+        public ApiModel.CfarContract UpdateCfarContractWithFormsOfPayment(string contractId, UpdateCfarContractFormsOfPaymentRequest request, string? sessionId)
+        {
+            var fops = new List<ApiModel.FormOfPayment>();
+            foreach (var p in request.FormsOfPayment)
+            {
+                var instance = p.ActualInstance;
+                if (instance.GetType() == typeof(Cash) || instance is Cash)
+                {
+                    var cash = p.GetCash();
+                    fops.Add(new ApiModel.FormOfPayment(new ApiModel.Cash(
+                        cash.Amount,
+                        cash.Currency,
+                        "cash"
+                    )));
+                }
+                else if (instance.GetType() == typeof(NonCash) || instance is NonCash)
+                {
+                    var nonCash = p.GetNonCash();
+                    fops.Add(new ApiModel.FormOfPayment(new ApiModel.NonCash(
+                        nonCash.Amount,
+                        nonCash.Currency,
+                        "non_cash"
+                    )));
+                }
+                else if (instance.GetType() == typeof(TokenizedPaymentCard) || instance is TokenizedPaymentCard)
+                {
+                    var card = p.GetTokenizedPaymentCard();
+                    fops.Add(new ApiModel.FormOfPayment(new ApiModel.PaymentCard(
+                        card.Amount,
+                        card.Currency,
+                        card.Token,
+                        "payment_card"
+                    )));
+                }
+                else if (instance.GetType() == typeof(PaymentCard) || instance is PaymentCard)
+                {
+                    var card = p.GetPaymentCard();
+                    var method = new PaymentMethod(new CreditCard(card.FirstName, card.LastName, card.Number, card.VerificationValue, card.Month, card.Year));
+                    method.Encrypt(this.Encryption);
+                    var tokenized = PaymentApi.PostCreditCard(new CreateCreditCardRequest(method));
+                    fops.Add(new ApiModel.FormOfPayment(new ApiModel.PaymentCard(
+                        card.Amount,
+                        card.Currency,
+                        tokenized.Transaction.PaymentMethod.Token,
+                        "payment_card"
+                    )));
+                }
+                else if (instance.GetType() == typeof(Points) || instance is Points)
+                {
+                    var points = p.GetPoints();
+                    fops.Add(new ApiModel.FormOfPayment(new ApiModel.Points(
+                        points.Amount,
+                        "points"
+                    )));
+                }
+            }
+            var paymentRequest = new ApiModel.UpdateCfarFormOfPaymentRequest(fops);
+            return CfarApi.PutCfarContractsIdFormsOfPayment(contractId, paymentRequest, sessionId);
+        }
+    }
+}
