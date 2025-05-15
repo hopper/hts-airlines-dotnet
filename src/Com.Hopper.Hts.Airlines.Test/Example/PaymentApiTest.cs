594f0449275fc153bf0e5620c4d2b426483e3878
Leonard Law <llaw@hopper.com>
2025-05-14 22:21:54 -0400

    chore: use tasks in payment api, cleanup

--- src/Com.Hopper.Hts.Airlines.Test/Example/PaymentApiTest.cs
+++ src/Com.Hopper.Hts.Airlines.Test/Example/PaymentApiTest.cs
@@ -5,6 +5,7 @@ using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.Extensions.DependencyInjection;
 using Xunit.Sdk;
+using System.Threading.Tasks;
 
 namespace Example
 {
@@ -18,65 +19,49 @@ namespace Example
             var host = HostBuilderUtils.CreateSpreedlyHostBuilder().Build();
             var paymentApi = host.Services.GetRequiredService<IPaymentApi>();
 
-            try
-            {
-                var request = new CreatePaymentMethodRequest(
-                    new CreatePaymentMethod(
-                        new CreateCreditCard(
-                            "John",
-                            "Smith",
-                            "4111111111111111",
-                            "123",
-                            "01",
-                            "2028"
-                        )
+            var request = new CreatePaymentMethodRequest(
+                new CreatePaymentMethod(
+                    new CreateCreditCard(
+                        "John",
+                        "Smith",
+                        "4111111111111111",
+                        "123",
+                        "01",
+                        "2028"
                     )
-                );
-                var response = paymentApi.PostPaymentMethodAsync(request).Result.Created() ?? throw new Exception("Payment method tokenization failed");
-                var transaction = response.Transaction ?? throw new Exception("Creation failed");
-                Debug.Print("Transaction: " + transaction);
+                )
+            );
+            var response = paymentApi.PostPaymentMethodAsync(request).Result.Created() ?? throw new Exception("Payment method tokenization failed");
+            var transaction = response.Transaction ?? throw new Exception("Creation failed");
+            Debug.Print("Transaction: " + transaction);
 
-                Assert.IsNotNull(transaction?.PaymentMethod.Token);
-            }
-            catch (Com.Hopper.Hts.Airlines.Spreedly.Client.ApiException e)
-            {
-                Console.WriteLine("Uh oh: " + e);
-            }
-            // task.Dispose();
+            Assert.IsNotNull(transaction?.PaymentMethod.Token);
         }
 
         [TestMethod]
-        public void TokenizeWithEncryptionTest()
+        public async Task TokenizeWithEncryptionTest()
         {
             var host = HostBuilderUtils.CreateSpreedlyHostBuilder().Build();
             var paymentApi = host.Services.GetRequiredService<IPaymentApi>();
 
-            try
-            {
-                var request = new CreatePaymentMethodRequest(
-                    new CreatePaymentMethod(
-                        new CreateCreditCard(
-                            "John",
-                            "Smith",
-                            "4111111111111111",
-                            "123",
-                            "01",
-                            "2028"
-                        )
+            var request = new CreatePaymentMethodRequest(
+                new CreatePaymentMethod(
+                    new CreateCreditCard(
+                        "John",
+                        "Smith",
+                        "4111111111111111",
+                        "123",
+                        "01",
+                        "2028"
                     )
-                );
-                var encrypted = TestSecrets.Encryption.Encrypt(request);
-                var task = paymentApi.PostPaymentMethodAsync(encrypted);
-                var response = task.Result.Created() ?? throw new Exception("Payment method tokenization failed");
-                var transaction = response.Transaction ?? throw new Exception("Creation failed");
-                Debug.Print("Transaction: " + transaction);
+                )
+            );
+            var encrypted = TestSecrets.Encryption.Encrypt(request);
+            var response = (await paymentApi.PostPaymentMethodAsync(encrypted)).Created() ?? throw new Exception("Payment method tokenization failed");
+            var transaction = response.Transaction;
+            Debug.Print("Transaction: " + transaction);
 
-                Assert.IsNotNull(transaction?.PaymentMethod.Token);
-            }
-            catch (Com.Hopper.Hts.Airlines.Spreedly.Client.ApiException e)
-            {
-                Console.WriteLine("Uh oh: " + e);
-            }
+            Assert.IsNotNull(transaction.PaymentMethod.Token);
         }
     }
 }
